501fd9e222719d7ccccc35a6b880029bd0848abd
Arina Zubova <arina777pototskaya@gmail.com>
2023-03-01 18:44:14 +0400

    13

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from hexlet_django_blog.article.models import Article
@@ -10,4 +10,13 @@ class IndexView(View):
         articles = Article.objects.all()[:15]
         return render(request, 'article/index.html', context={
             'articles': articles,
+        })
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'article/show.html', context={
+            'article': article
         })
